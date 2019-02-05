e32977878620e3a01df46b56e481d5aaf5dd112a
Knut Erik Hollund <knut.erik@unlike.no>
2019-02-05 20:45:51 +0100

    Changed header banner

--- web/banner.php
+++ web/banner.php
@@ -1,7 +1,7 @@
 <div class="container-fluid TKBanner" >
     <div class="row">
         <div class="col-xs-12"> 
-            <img class="img-responsive" src="./img/tkmanx_header.png" alt="None">
+            <img class="img-responsive" src="./img/tkmanx_header.jpg" alt="None">
         </div>
     </div>
 </div>
