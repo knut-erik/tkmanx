59429f766bf0a69e1b11789788797c71165791b3
Knut Erik Hollund <knut.erik@unlike.no>
2019-07-24 15:16:20 +0200

    Added betingelsr and paamelding files for the 2020 tour

---
+++ web/md/2020_paamelding.md
@@ -0,0 +1,6 @@
+# Påmelding
+
+Påmeldingen åpner **1.august.2019 - kl. 18.00**
+
+- For at du skal kunne være godt forberedt til påmeldingen, kan du lese igjennom spørsmålene du må besvare i registreringen her [(PDF fil med spørsmålene.)](https://drive.google.com/file/d/1OFhvyW5bAlsmxDPQebaYJdeiLF_en0Q9/view?usp=sharing)
+- **Lenke til Påmeldingsskjema kommer!**
--- web/tab_2020bc.php
+++ web/tab_2020bc.php
@@ -1,7 +1,16 @@
 <div id="2020" class="tab-pane fade TKPanel">
     <div class="panel panel-default TKPanel">
         <div class="panel-body TKPanel">
+            <?php echo $Parsedown->text(file_get_contents('./md/2020_paamelding.md'));  ?>
+        </div>
+    </div><div class="panel panel-default TKPanel">
+        <div class="panel-body TKPanel">
             <?php echo $Parsedown->text(file_get_contents('./md/2020bc.md'));  ?>
+        </div>
+    </div>
+    <div class="panel panel-default TKPanel">
+        <div class="panel-body TKPanel">
+            <?php echo $Parsedown->text(file_get_contents('./md/2020_betingelser.md'));  ?>
         </div>
     </div>
 </div>
