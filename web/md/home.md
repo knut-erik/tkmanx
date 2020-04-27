e2459ff7cc26f466daac65e54698334fb287a639
knut-erik <knut.erik@hollund.org>
2020-04-27 20:36:36 +0200

    Bogus href links - removed

--- web/md/home.md
+++ web/md/home.md
@@ -1,4 +1,4 @@
 ### Velg en av toktene for mer informasjon:
 
-- <a  href="#" onClick="populateBC();" data-toggle="tab">2020 - Arrangør Broken Chains JC</a>
+- <a  href="" onClick="populateBC();" data-toggle="tab">2020 - Arrangør Broken Chains JC</a>
 - 2021 - Kommer ...
--- web/menu.php
+++ web/menu.php
@@ -6,12 +6,12 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand TKMenuText" data-toggle="tab" onClick="populateHome();" href="#">Tokt til Isle of Man</a>
+            <a class="navbar-brand TKMenuText" data-toggle="tab" onClick="populateHome();" href="">Tokt til Isle of Man</a>
         </div>
         <div class="collapse navbar-collapse" id="tknavbar">
             <ul class="nav navbar-nav TKFont TKMenuText">
-                <li><a class="TKMenuText" data-toggle="tab" onClick="populateBC();" href="#">2020</a></li>
-                <li><a class="TKMenuText" data-toggle="tab" onClick="populateLinks();" href="#">Lenker</a></li>
+                <li><a class="TKMenuText" data-toggle="tab" onClick="populateBC();" href="">2020</a></li>
+                <li><a class="TKMenuText" data-toggle="tab" onClick="populateLinks();" href="">Lenker</a></li>
             </ul>
         </div>
     </div>
