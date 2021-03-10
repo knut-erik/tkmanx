fcde89a79ed966fc70daaddfaf1d2ee7215d839b
knut-erik <knut.erik@hollund.org>
2021-03-10 16:23:01 +0100

    version bump

--- web/sw.js
+++ web/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.0/workbox-sw.js');
 
-let version = "25022021-1836";
+let version = "10032021-1622";
 
 if (workbox) {
   console.log(`Workbox is loaded`);
